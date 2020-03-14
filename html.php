84e0873af948f7e9e483198b0cfc1e472363ed0e
putin <help.injective@gmail.com>
2020-03-14 16:34:52 +0300

    Update html.php

--- html.php
+++ html.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once("https://ingective.github.io/HTMLToPHP/html_core.php");
-require_once("https://ingective.github.io/HTMLToPHP/html_label.php");
+require_once("https://ingective.github.io/HTMLToPHP/html_label.php"); 
 require_once("https://ingective.github.io/HTMLToPHP/html_layout.php");
 require_once("https://ingective.github.io/HTMLToPHP/html_document.php");
 
