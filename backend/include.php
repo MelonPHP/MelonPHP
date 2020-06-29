d81901893d41abb1ce0339dbbf95d1df9cd2f87b
Roman S <ua.romansyslikov@gmail.com>
2020-06-29 21:56:22 +0300

    [TRUE] Update site to GidraPHP 4.0.1

--- backend/include.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once(__DIR__ . "/controllers/include.php");
-require_once(__DIR__ . "/models/include.php");
-//require_once(__DIR__ . "/");
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once(__DIR__ . "/backend/include.php");
-
-Controller::RedirectTo("ui/pages/HomePage.php");
