1ca2d3d39244ef87897b644e5554fdf76becd3d8
rovecode <ua.romansyslikov@gmail.com>
2021-02-06 23:32:42 +0200

    Now Display Widget must return Scaffold only

--- src/common/display_widget.php
+++ src/common/display_widget.php
@@ -1,6 +1,8 @@
 <?php
 
 abstract class DisplayWidget extends ConfigureWidget {
+    public abstract function build() : Scaffold;
+
     public function display() {
         echo $this->createElement()->paint();
     }
