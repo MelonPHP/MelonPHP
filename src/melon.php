4a6d8981b0291826fcd4a7a1bf135b9d599d286e
rovecode <ua.romansyslikov@gmail.com>
2021-02-07 03:47:36 +0200

    Add routers and Controllers

--- src/melon.php
+++ src/melon.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/common.php';
+require_once __DIR__ . '/controllers.php';
 require_once __DIR__ . '/enums.php';
 require_once __DIR__ . '/paints.php';
 require_once __DIR__ . '/styles.php';
--- src/routers/router.php
+++ src/routers/router.php
@@ -3,17 +3,17 @@
 class Route {
     private static Array $routes = [];
   
-    public static function add(String $path, $function){
+    public static function add(String $path, $function) : void {
         self::$routes[$path] = $function;
     }
 
-    public static function run(String $init, String $notFind = null) {
+    public static function run(String $init = "/", String $notFind = null) : void {
         $parsed_url = parse_url($_SERVER['REQUEST_URI']);
 
         if(isset($parsed_url['path'])){
             $path = $parsed_url['path'];
         } else {
-            $path = '/';
+            $path = $init;
         }
 
         foreach (self::$routes as $key => $value) {
@@ -27,5 +27,14 @@ class Route {
             self::$routes[$notFind]()->display();
         }
     }
+
+    public static function move(String $path = "/") {
+        header('Location:'.$path);
+        self::rip();
+    }
+
+    public static function rip() : void {
+        die();
+    }
   
-  }
+}
